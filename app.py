e7324b8d4315aacaea9330f3262aed4a667ed021
Dmitry Strunets <d.strunetss@gmail.com>
2025-05-10 17:48:48 +0300

    [FIX:css for caht-bot]

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from utils.nika_utils import get_nika_response
 from utils.html_parser import html_to_text
 app = Flask(__name__)
-## <iframe src="http://ваш-сервер:5000" width="350" height="500"></iframe> - добавление на целевые веб-страницы
+## <iframe src="http://localhost:5000" width="350" height="500"></iframe> - добавление на целевые веб-страницы
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.json.get('message')
